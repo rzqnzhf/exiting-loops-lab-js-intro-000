79843bd86d0cc2a33bfe4f76532078b1a0f56217
Skin <rizqinnazhif@gmail.com>
2020-01-01 04:38:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ function breakOut(array, changeValue, stopValue) {
   for (let i = 0; i < array.length; i++) {
     array[i] = changeValue;
     if (array[i] === stopValue) {
-      return array;
+      break;
     }
+    return array;
   }
 }
