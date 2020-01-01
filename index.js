1fdf1cbcee2a5b635b697a09568b1606f70a4aa4
Skin <rizqinnazhif@gmail.com>
2020-01-01 04:46:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ function breakOut(array, changeValue, stopValue) {
     }
   }
 }
+
+function keepGoing(array, changeValue, skipValue) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === skipValue) {
+      continue;
+    }
+    array[i] = changeValue;
+  }
+  return array;
+}
+
+
+
+
+
+
