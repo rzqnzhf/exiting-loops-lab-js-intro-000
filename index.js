729bf30ba716c344586d66ca504d07881c0bcb65
Skin <rizqinnazhif@gmail.com>
2020-01-01 04:54:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,12 +21,7 @@ function keepGoing(array, changeValue, skipValue) {
 function findBy(array, findFn) {
   for (let i = 0; i < array.length; i++) {
     if (findFn(array[i])) {
-      
+
     }
   }
 }
-
-
-
-
-
